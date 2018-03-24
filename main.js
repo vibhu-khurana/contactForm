bab8a84508cfe9985f9b7535adda4639c21a018f
vibhu-khurana <kkhurana.vibhu@gmail.com>
2018-03-24 08:39:00 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyBJMXnVY-55x0yrr-IDmlYbDpHd4JCBwsg",
+    authDomain: "vibsform.firebaseapp.com",
+    databaseURL: "https://vibsform.firebaseio.com",
+    projectId: "vibsform",
+    storageBucket: "",
+    messagingSenderId: "463948333610"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection, used to initialise firebase db
+var messagesRef = firebase.database().ref('messages');
+// messages is a collection
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function() {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+}
